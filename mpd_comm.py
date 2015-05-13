fc6dcba0b9e79896d5c9b03b33306d5b6643dd42
Nils Noreyson <NilsNoreyson@gmail.com>
2015-05-13 09:26:30 +0200

    debug of the folderlisting

--- mpd_comm.py
+++ mpd_comm.py
@@ -85,7 +85,7 @@ def get_spotify_playlists():
     client.password('IlPits2013')
     folders = client.listall('Spotify')
     #folders = client.lsinfo('Spotify')
-    #folders = [f['directory'].split(r'/')[1] for f in folders if 'directory' in f.keys()]
+    folders = [f['directory'].split(r'/')[1] for f in folders if 'directory' in f.keys()]
     #folders = [f['directory'].split(r'/') for f in folders if 'directory' in f.keys()]
     #folders = [f[1] for f in folders if len(f)==2]
     client.disconnect()

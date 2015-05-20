14989ea3c92991ae8e1549379391b42ca795aeb4
root <root@Marvin.lan>
2015-05-21 01:13:22 +0200

    script to sync the folders

--- sync_script.sh
+++ sync_script.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 rsync -rav root@192.168.13.13:/media/phonebook /media/
 rsync -rav --delete root@192.168.13.13:/media/playlists /media/
+/var/Projects/PhoneBookServer/venv/bin/python /var/Projects/PhoneBookServer/phoneBookServer/get_phonebook_files.py
 
 rsync -rvl --delete --files-from=/media/satellite_mpds/files2sync.txt /media/music /media/satellite_mpds/music
 

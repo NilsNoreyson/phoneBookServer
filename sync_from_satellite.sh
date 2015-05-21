dcf375b57235563f19591a247c72b7ae2443eb12
petraMBox <peterbonanati@gmail.com>
2015-05-21 10:40:41 +0000

    do the datasync over port 2213 by ssh, not over the openvpn, since the powerline connection in between seems to fuckup things

--- sync_from_satellite.sh
+++ sync_from_satellite.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-rsync -avz --delete -e ssh treize@treize.duckdns.org:/media/satellite_mpds/music/ /media/music
-rsync -avz --delete -e ssh treize@treize.duckdns:/media/satellite_mpds/phonebook/ /media/phonebook
-rsync -avz --delete -e ssh treize@treize.duckdns:/media/satellite_mpds/playlists/ /media/playlists
+ssh_port=2213
+rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns.org:/media/satellite_mpds/music/ /media/music
+rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns:/media/satellite_mpds/phonebook/ /media/phonebook
+rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns:/media/satellite_mpds/playlists/ /media/playlists
 
 mkdir /media/mpd
 chown -R mpd:mpd /media/
 mpc update 
-
+systemctl restart telFlask

2657f7edf77795ca3c0e6e0fb8b282957058d45c
Peter Bonanati <peter.bonanati@ipc.uni-tuebingen.de>
2015-05-27 13:15:23 +0200

    dns name in sync script was not correct

--- sync_from_satellite.sh
+++ sync_from_satellite.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ssh_port=2213
 rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns.org:/media/satellite_mpds/music/ /media/music
-rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns:/media/satellite_mpds/phonebook/ /media/phonebook
-rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns:/media/satellite_mpds/playlists/ /media/playlists
+rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns.org:/media/satellite_mpds/phonebook/ /media/phonebook
+rsync -avz --delete -e "ssh -p $ssh_port" treize@treize.duckdns.org:/media/satellite_mpds/playlists/ /media/playlists
 
 mkdir /media/mpd
 chown -R mpd:mpd /media/
